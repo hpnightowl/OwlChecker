9a4357710a5d1c7f14eb541a8f5503ff1beac9c3
hpnightowl <iamhp2k@gmail.com>
2022-05-31 15:25:10 +0530

    owlchecker: make it more modular

--- owlseye.py
+++ owlseye.py
@@ -2,15 +2,22 @@ import argparse
 import pandas as pd
 # it is a version check why owlseye name? because it will look for the latest version of the program continuously 
 
+# fetches json data
+def fetch_pkg():
+    pass
+
+# gets the csv data
+def get_data(args):
+    data = pd.read_csv(args.input[0])
+    return data
+
+# converts the data from input file
+def data_prep(data):
+    pass
+
+# Where cooking begins
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="This is a program to check the version of the program")
     parser.add_argument("-i", "--input", help="input file", required=True, nargs=2)
     args = parser.parse_args()
-    data = pd.read_csv(args.input[0])
-    check_pkg = args.input[1]
-    print(check_pkg)
-    repo_link = data['repo']
-    repo_name = data['name']
-
-print("Repo Link: " + repo_link)
-print("Repo Name: " + repo_name)
+    data = get_data(args)
