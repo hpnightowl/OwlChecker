b8b0c962bb8e07f84861529677a49add9dd96361
hpnightowl <iamhp2k@gmail.com>
2022-05-31 14:55:24 +0530

    owlchecker: init CLI args

--- owlseye.py
+++ owlseye.py
@@ -1,13 +1,16 @@
-import sys
+import argparse
 import pandas as pd
 # it is a version check why owlseye name? because it will look for the latest version of the program continuously 
 
-input_file = sys.argv[1]
-
-data = pd.read_csv(input_file)
-
-repo_link = data['repo']
-repo_name = data['name']
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="This is a program to check the version of the program")
+    parser.add_argument("-i", "--input", help="input file", required=True, nargs=2)
+    args = parser.parse_args()
+    data = pd.read_csv(args.input[0])
+    check_pkg = args.input[1]
+    print(check_pkg)
+    repo_link = data['repo']
+    repo_name = data['name']
 
 print("Repo Link: " + repo_link)
 print("Repo Name: " + repo_name)
