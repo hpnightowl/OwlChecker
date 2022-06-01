976e2db70b5c3c43d0a6453bb406ae65a48926b3
hpnightowl <iamhp2k@gmail.com>
2022-06-01 21:01:58 +0530

    [WIP]owlchecker: github post request API

--- owlseye.py
+++ owlseye.py
@@ -1,6 +1,62 @@
 import argparse
 import pandas as pd
+import git
+from github import Github
+import requests
+import json
 # it is a version check why owlseye name? because it will look for the latest version of the program continuously 
+
+# save only the false results location and make changes in that
+save_false_i = []
+
+# github api
+def github(args):
+    data = get_data(args)
+    for i in save_false_i:
+        s = data['repo'][i]
+        s = s.replace("github.com", "raw.githubusercontent.com")
+        s+= "/master/package.json"
+        read_json = pd.read_json(s)
+        
+        # new_repo = git.Repo.init(data["name"][i])
+        # my_repo = git.Repo(data["name"][i])
+        # file_name = wget.download(s, data["name"][i])
+        # g = Github("token")
+        # user = g.get_user()
+        # #repo = user.create_repo(data["name"][i])
+        # git.Repo.clone_from(data['repo'][i], data['name'][i])
+        # repo = git.Repo(data['name'][i])
+        # with git.Repo.init(data["name"][i]).config_writer() as git_config:
+        #     git_config.set_value('user', 'email', 'proaf@gmail.com')
+        #     git_config.set_value('user', 'name', 'proaf')
+        # git.Repo(data['name'][i])
+        # read_json = pd.read_json(data["name"][i]+"/package.json")
+        # read_json['dependencies'][args.input[1].split('@')[0]] = args.input[1].split('@')[1]
+        # result = read_json.to_json(data['name'][i]+"/package.json")
+        # repo.git.branch('update')
+        # repo.git.checkout('update')
+        # repo.index.add("package.json")
+        # repo.index.commit("update version")
+        # # # Create a new remote
+        # remote = repo.create_remote('update', url='https://github.com/proaf/'+data["name"][i])
+        # repo.remotes.update.push()
+
+        headers={
+            "Authorization": "token token_id",
+            "Accept": "application/vnd.github.v3+json"
+        }
+        gitdata={
+            "title": "update version",
+            "body": "update version",
+            "head": "update",
+            "base": "master"
+        }
+        username = "proaf"
+        repo_name = data["name"][i]
+        #print(data['name'][i])
+        repo_url = "https://api.github.com/repos/"+username+"/"+repo_name+"/pulls"
+        response = requests.post(repo_url, data=json.dumps(gitdata), headers=headers)
+        print(response.json())
 
 def compare_version(version,args):
     satisfied = []
@@ -9,6 +65,7 @@ def compare_version(version,args):
             satisfied.append(True)
         else:
             satisfied.append(False)
+            save_false_i.append(i)
     return satisfied
 
 # fetches json data
@@ -43,7 +100,7 @@ def save_results(version, version_satisfied, args):
 
 # Where cooking begins
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="This is a program to check the version of the program")
+    parser = argparse.ArgumentParser()
     parser.add_argument("-i", "--input", help="input file", required=True, nargs=2)
     args = parser.parse_args()
     data = get_data(args)
@@ -51,3 +108,4 @@ if __name__ == "__main__":
     version = fetch_pkg(len(data),url,args)
     version_satisfied = compare_version(version,args)
     save_results(version, version_satisfied,args)
+    github(args)
